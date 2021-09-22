50ea59b89764685aa08ec8fb1b66147216a6f9b6
Ivqonnada Al Mufarrih <36329890+ivqonsanada@users.noreply.github.com>
2021-09-22 01:09:39 +0000

    fix: wrong default target directory path

--- cjp.sh
+++ cjp.sh
@@ -5,8 +5,6 @@ ARGS=($@)
 PROJECT_NAME=${ARGS[0]} || false
 PROJECT_NAME=`echo $PROJECT_NAME | sed 's/\/$//g'`
 TARGET_DIRECTORY=${ARGS[1]} || false
-TARGET_DIRECTORY=`echo $TARGET_DIRECTORY | sed 's/\/$//g'`
-FULL_PATH=$TARGET_DIRECTORY/$PROJECT_NAME
 
 function initProject() {
   if [[ -z $PROJECT_NAME ]]; then
@@ -15,24 +13,25 @@ function initProject() {
       exit 1
   fi
 
-  if [ ! -d $FULL_PATH ]
-  then
-      mkdir -p $FULL_PATH
-  fi
-
   echo "INFO: Copying template.."
 
   if [[ -z $TARGET_DIRECTORY ]]; then
       echo "INFO: Target directory is not provided using current folder as target."
-      cp -R template/* ./$PROJECT_NAME
+      cp -R template/ ./$PROJECT_NAME
       echo "INFO: Initializing git repo"
       cd ./$PROJECT_NAME
       chmod +x gradlew 
       git init 
       git add .
   else
+      TARGET_DIRECTORY=`echo $TARGET_DIRECTORY | sed 's/\/$//g'`
+      FULL_PATH=$TARGET_DIRECTORY/$PROJECT_NAME
+      if [ ! -d $FULL_PATH ]
+      then
+          mkdir -p $FULL_PATH
+      fi
       echo "INFO: Creating new project at ${TARGET_DIRECTORY}"
-      cp -R template/* $TARGET_DIRECTORY/$PROJECT_NAME
+      cp -R template/ $TARGET_DIRECTORY/$PROJECT_NAME
       echo "INFO: Initializing git repo"
       cd $TARGET_DIRECTORY/$PROJECT_NAME
       chmod +x gradlew 
