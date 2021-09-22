004bf5ce890e54b9cea2c13fb9fa3b62603b71f5
Ivqonnada Al Mufarrih <36329890+ivqonsanada@users.noreply.github.com>
2021-09-22 00:27:17 +0000

    fix: doesn't create non-existing target directory

--- cjp.sh
+++ cjp.sh
@@ -3,7 +3,10 @@
 ARGS=($@)
 
 PROJECT_NAME=${ARGS[0]} || false
+PROJECT_NAME=`echo $PROJECT_NAME | sed 's/\/$//g'`
 TARGET_DIRECTORY=${ARGS[1]} || false
+TARGET_DIRECTORY=`echo $TARGET_DIRECTORY | sed 's/\/$//g'`
+FULL_PATH=$TARGET_DIRECTORY/$PROJECT_NAME
 
 function initProject() {
   if [[ -z $PROJECT_NAME ]]; then
@@ -12,11 +15,16 @@ function initProject() {
       exit 1
   fi
 
+  if [ ! -d $FULL_PATH ]
+  then
+      mkdir -p $FULL_PATH
+  fi
+
   echo "INFO: Copying template.."
 
   if [[ -z $TARGET_DIRECTORY ]]; then
       echo "INFO: Target directory is not provided using current folder as target."
-      cp -r template ./$PROJECT_NAME
+      cp -R template/* ./$PROJECT_NAME
       echo "INFO: Initializing git repo"
       cd ./$PROJECT_NAME
       chmod +x gradlew 
@@ -24,7 +32,7 @@ function initProject() {
       git add .
   else
       echo "INFO: Creating new project at ${TARGET_DIRECTORY}"
-      cp -r template $TARGET_DIRECTORY/$PROJECT_NAME
+      cp -R template/* $TARGET_DIRECTORY/$PROJECT_NAME
       echo "INFO: Initializing git repo"
       cd $TARGET_DIRECTORY/$PROJECT_NAME
       chmod +x gradlew 
